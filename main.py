d54144b24c80b757878e20b8784ae19f00f791c2
TheSpeedX <ggspeedx29@gmail.com>
2022-04-04 23:13:14 +0530

    restructure to OOP approach

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-import time
-
 import pygame
-import random
 import sounddevice as sd
 import numpy as np
+
+import time
+import random
 import os
 
 black = (0, 0, 0)
@@ -18,150 +18,219 @@ surface = pygame.display.set_mode((surfaceWidth, surfaceHeight))
 pygame.display.set_caption('Noisy Bird')
 clock = pygame.time.Clock()
 
-img = pygame.image.load(os.path.join('images', 'bird.png'))
-img_width = img.get_size()[0]
-img_height = img.get_size()[1]
 
-die_sound = pygame.mixer.Sound(os.path.join('sounds', 'die.mp3'))
+class Bird:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.img = pygame.image.load(os.path.join('images', 'bird.png'))
+        self.img_width = self.img.get_size()[0]
+        self.img_height = self.img.get_size()[1]
+        self.jump = 5
+        self.gravity = 5
+        self.die_sound = pygame.mixer.Sound(os.path.join('sounds', 'die.mp3'))
 
+    def draw(self):
+        surface.blit(self.img, (self.x, self.y))
 
-def show_score(current_score):
-    font = pygame.font.Font('freesansbold.ttf', 20)
-    text = font.render(f'Score:{current_score}', True, white)
-    surface.blit(text, [3, 3])
+    def move_up(self):
+        self.y -= self.jump
 
+    def move_down(self):
+        self.y += self.gravity
 
-def blocks(x_block, y_block, block_width, block_height, gap):
-    pygame.draw.rect(surface, green, [
-                     x_block, y_block, block_width, block_height])
-    pygame.draw.rect(surface, green, [
-                     x_block, y_block + block_height + gap,
-                     block_width, surfaceHeight])
-
-
-def makeTextObjs(text, font):
-    textSurface = font.render(text, True, white)
-    return textSurface, textSurface.get_rect()
-
-
-def replay_or_quit():
-    for event in pygame.event.get([pygame.KEYDOWN, pygame.KEYUP, pygame.QUIT]):
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            exit()
-
-        elif event.type == pygame.KEYDOWN:
-            continue
-
-        return event.key
-    return None
+    def is_out_of_bounds(self):
+        return (self.y > surfaceHeight - self.img_height or self.y < 0)
 
 
-def msg_surface(text):
-    smallText = pygame.font.Font('freesansbold.ttf', 20)
-    largeText = pygame.font.Font('freesansbold.ttf', 130)
+class ScoreCard:
+    def __init__(self):
+        self.position = [3, 3]
+        self.score = 0
+        self.font = pygame.font.Font('freesansbold.ttf', 20)
 
-    titletextSurf, titleTextRect = makeTextObjs(text, largeText)
-    titleTextRect.center = surfaceWidth / 2, surfaceHeight / 2
-    surface.blit(titletextSurf, titleTextRect)
+    def draw(self):
+        # TODO: Show High Score Aswell
+        text = self.font.render(f'Score: {self.score}', True, white)
+        surface.blit(text, self.position)
 
-    typtextSurf, typTextRect = makeTextObjs(
-        'Press any key to continue', smallText)
-    typTextRect.center = surfaceWidth / 2, ((surfaceHeight / 2) + 100)
-    surface.blit(typtextSurf, typTextRect)
-
-    pygame.display.update()
-    time.sleep(1)
-
-    while replay_or_quit() is None:
-        clock.tick()
-
-    main()
+    def update(self, score):
+        self.score = score
 
 
-def gameOver():
-    die_sound.play()
-    msg_surface('Game over')
+class Block:
+    def __init__(self, block_width, block_height, gap):
+        self.x_block = surfaceWidth
+        self.y_block = 0
+        self.block_width = block_width
+        self.block_height = block_height
+        self.gap = gap
+        self.passed = False
+
+    def draw(self):
+        pygame.draw.rect(
+            surface, green,
+            [
+                self.x_block, self.y_block,
+                self.block_width, self.block_height
+            ]
+        )
+        pygame.draw.rect(
+            surface, green,
+            [
+                self.x_block, self.y_block + self.block_height + self.gap,
+                self.block_width, surfaceHeight
+            ]
+        )
+
+    def move(self, x, y):
+        self.x_block = x
+        self.y_block = y
+
+    def update(self, gap):
+        self.gap = gap
+
+    def check_passed(self, bird: Bird):
+        return (
+            bird.x > self.x_block + self.block_width and
+            bird.x < self.x_block + self.block_width + bird.img_width / 5
+        )
+
+    def check_collision(self, bird: Bird):
+        return (
+            (
+                bird.y < self.block_height
+                or bird.y + bird.img_height > self.block_height + self.gap
+            )
+            and bird.x + bird.img_width > self.x_block
+            and bird.x < self.x_block + self.block_width
+        )
 
 
-def bird(x, y, image):
-    surface.blit(image, (x, y))
+class TextBlock:
+    def __init__(self, text, size=20):
+        self.font = pygame.font.Font('freesansbold.ttf', size)
+        self.text = self.font.render(text, True, white)
+
+    def center_text(self, x, y):
+        self.text.get_rect().center = (x, y)
+
+    def draw(self, position):
+        surface.blit(self.text, position)
 
 
-y = 200
+class NoisyBird:
 
+    bird = Bird(150, 200)
+    score_card = ScoreCard()
+    block = Block(50, random.randint(0, surfaceHeight / 2), bird.img_height*5)
 
-def process_sound(indata, outdata, frames, time, status):
-    global y
-    volume_norm = np.linalg.norm(indata)
-    if volume_norm > 1:
-        y -= 5
-    else:
-        y += 5
+    def __init__(self):
+        self.game_over = False
 
+    @staticmethod
+    def process_sound(indata, outdata, frames, time, status):
+        volume_norm = np.linalg.norm(indata)
+        if volume_norm > 1:
+            NoisyBird.bird.move_up()
+        else:
+            NoisyBird.bird.move_down()
 
-def main():
-    global y
-    x = 150
-    y = 200
+    @staticmethod
+    def reset_game():
+        NoisyBird.bird = Bird(150, 200)
+        NoisyBird.score_card = ScoreCard()
+        NoisyBird.block = Block(50, random.randint(
+            0, surfaceHeight / 2), NoisyBird.bird.img_height*5)
 
-    x_block = surfaceWidth
-    y_block = 0
-
-    block_width = 50
-    block_height = random.randint(0, surfaceHeight / 2)
-    gap = img_height * 5
-
-    # speed of blocks
-    block_move = 3
-
-    score = 0
-    game_over = False
-
-    # Game Loop
-    while not game_over:
-
-        for event in pygame.event.get():
+    def replay_or_quit(self):
+        for event in pygame.event.get(
+            [pygame.KEYDOWN, pygame.KEYUP, pygame.QUIT]
+        ):
             if event.type == pygame.QUIT:
-                game_over = True
+                pygame.quit()
+                exit()
 
-        surface.fill(blue)
-        bird(x, y, img)
-        show_score(score)
+            elif event.type == pygame.KEYDOWN:
+                continue
 
-        blocks(x_block, y_block, block_width, block_height, gap)
-        x_block -= block_move
+            return event.key
+        return None
 
-        # boundaries
-        if y > surfaceHeight - img_height or y < 0:
-            gameOver()
-
-        # blocks on screen or not
-        if x_block < (-1 * block_width):
-            x_block = surfaceWidth
-            block_height = random.randint(0, surfaceHeight / 2)
-
-        # Collision Detection
-
-        if (
-            x + img_width > x_block
-            and x < x_block + block_width
-            and (y < block_height or y + img_height > block_height + gap)
-        ):
-            gameOver()
-
-        # detecting whether we are past the block or not in X
-        if (
-            x > x_block + block_width and
-            x < x_block + block_width + img_width / 5
-        ):
-            score += 1
-
+    def game_over_screen(self):
+        game_over_text = TextBlock('GAME OVER', 70)
+        game_over_rect = game_over_text.text.get_rect()
+        game_over_rect.center = surfaceWidth / 2, (surfaceHeight / 2) - 50
+        continue_text = TextBlock('Press any key to continue', 20)
+        continue_rect = continue_text.text.get_rect()
+        continue_rect.center = surfaceWidth / 2, (surfaceHeight / 2) + 50
+        game_over_text.draw(game_over_rect)
+        continue_text.draw(continue_rect)
         pygame.display.update()
-        clock.tick(80)
+        time.sleep(1)
+
+        while self.replay_or_quit() is None:
+            clock.tick()
+        self.game_over = False
+        self.main()
+
+    def gameOver(self):
+        NoisyBird.bird.die_sound.play()
+        self.game_over_screen()
+
+    def main(self):
+        NoisyBird.reset_game()
+        print('Game Started')
+        x_block = surfaceWidth
+        y_block = 0
+
+        block_width = 50
+
+        # speed of blocks
+        block_move = 3
+
+        score = 0
+
+        # Game Loop
+        while not self.game_over:
+
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    self.game_over = True
+
+            surface.fill(blue)
+            NoisyBird.bird.draw()
+            NoisyBird.score_card.draw()
+
+            NoisyBird.block.move(x_block, y_block)
+            NoisyBird.block.draw()
+            x_block -= block_move
+
+            # boundaries
+            if NoisyBird.bird.is_out_of_bounds():
+                self.gameOver()
+
+            # blocks on screen or not
+            if x_block < (-1 * block_width):
+                x_block = surfaceWidth
+                NoisyBird.block.block_height = random.randint(
+                    0, surfaceHeight / 2)
+
+            # Collision Detection
+            if NoisyBird.block.check_collision(NoisyBird.bird):
+                self.gameOver()
+
+            # detecting whether we are past the block or not in X
+            if NoisyBird.block.check_passed(NoisyBird.bird):
+                score += 1
+                NoisyBird.score_card.update(score)
+
+            pygame.display.update()
+            clock.tick(80)
 
 
-sd.Stream(callback=process_sound).start()
-main()
+sd.Stream(callback=NoisyBird.process_sound).start()
+game = NoisyBird()
+game.main()
 pygame.quit()
 exit()
